fc11b394986e8fe272a642c66e44e8210817734b
Diya Patel <117513637+DiyaPatel25@users.noreply.github.com>
2024-04-09 00:16:27 +0530

    Again25.java

---
+++ Again25.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Again25 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BigInteger n=sc.nextBigInteger();
+        // int multi=1;
+        // for(int i=0;i<n;i++)
+        // {
+        //     multi=multi*5;
+        // }
+        System.out.println("25");
+        
+    }
+}
